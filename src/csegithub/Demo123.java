079e6b736a9af59df033611f059eec3472a77e3a
Parimala <Parimala@DESKTOP-QFTTCVA>
2022-10-16 10:01:46 -0700

    1st time commit

---
+++ src/csegithub/Demo123.java
@@ -0,0 +1,11 @@
+package csegithub;
+
+public class Demo123 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("welcome to devops lab");
+
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author Parimala
+ *
+ */
+module csegithub {
+}
