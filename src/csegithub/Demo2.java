0092644e1d04f52a8bbcf80c1be728804d4500e7
parimalaMeneddi <115864493+parimalaMeneddi@users.noreply.github.com>
2022-10-17 07:03:20 -0700

    hello

--- src/csegithub/Demo2.java
+++ src/csegithub/Demo2.java
@@ -5,6 +5,7 @@ public class Demo2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
            System.out.print("Nothing lasts forever");
+		System.out.println("Move towards to reach your goals");
 	}
 
 }
